ed016e31dfa4a3fd4db729e341e2eedda75159af
kwar <kaveh_tehrani@yahoo.com>
2023-08-11 00:41:47 -0400

    fix: umami

--- components/analytics/Umami.tsx
+++ components/analytics/Umami.tsx
@@ -2,5 +2,11 @@ import Script from 'next/script'
 import { siteMetadata } from '~/data/siteMetadata'
 
 export function UmamiScript() {
-  return <Script async data-website-id={siteMetadata.analytics.umamiWebsiteId} src="" />
+  return (
+    <Script
+      async
+      src="https://analytics.umami.is/script.js"
+      data-website-id={siteMetadata.analytics.umamiWebsiteId}
+    />
+  )
 }
