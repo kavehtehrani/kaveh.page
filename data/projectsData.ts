1fa39e5e7ea04f94bfe506d4b62dbf1ba44361e8
kavehtehrani <kaveh_tehrani@yahoo.com>
2024-09-12 11:44:52 +0300

    feat: one-click image saver extensions

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -40,6 +40,17 @@ export let projectsData: Project[] = [
   },
   {
     type: 'work',
+    title: 'One-Click Image Saver',
+    description: `A learning project in developing browser extensions.
+    
+    Save any image in your browser with one click via shortcut or contextual menu.`,
+    imgSrc: '/static/images/one_click_image_saver_context_menu.png',
+    url: 'https://chromewebstore.google.com/detail/one-click-image-saver/ajpjioafnelcifjpgeekhbcjpphhfmgg',
+    repo: 'one_click_image_save',
+    builtWith: ['JavaScript'],
+  },
+  {
+    type: 'work',
     title: 'Traffic Signal',
     description: `My very first web app! Traffic control for shared spaces.  Wrote this during pandemic lockdown to coordinate with my roommates when it's OK to enter their space, especially people who are working in areas without a door such as the living room.
 
