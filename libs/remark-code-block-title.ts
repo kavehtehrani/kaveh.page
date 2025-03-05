2a412565a0d96cb8802d5bf680408207019c826e
kavehtehrani <kaveh_tehrani@yahoo.com>
2025-03-05 18:18:40 +0800

    fix: vercel errors

--- libs/remark-code-block-title.ts
+++ libs/remark-code-block-title.ts
@@ -1,31 +1,26 @@
 import { visit } from 'unist-util-visit'
-import type { UnistNodeType, UnistTreeType } from '~/types'
+import type { Node } from 'unist'
+
+type CodeNode = Node & {
+  type: 'code'
+  lang?: string
+  meta?: string
+  value: string
+}
 
 export function remarkCodeBlockTitle() {
-  return (tree: UnistTreeType) => {
-    return visit(tree, 'code', (node: UnistNodeType, index) => {
+  return (tree: Node) => {
+    return visit(tree, 'code', (node: CodeNode) => {
       let nodeLang = node.lang || ''
       let language = ''
       let title = ''
 
       if (nodeLang.includes(':')) {
-        language = nodeLang.slice(0, nodeLang.search(':'))
-        title = nodeLang.slice(nodeLang.search(':') + 1, nodeLang.length)
+        language = nodeLang.slice(0, nodeLang.indexOf(':'))
+        title = nodeLang.slice(nodeLang.indexOf(':') + 1)
+        node.lang = language
+        node.meta = title
       }
-      if (!title) return
-
-      let className = 'remark-code-title'
-      let titleNode = {
-        type: 'mdxJsxFlowElement',
-        name: 'div',
-        attributes: [{ type: 'mdxJsxAttribute', name: 'className', value: className }],
-        children: [{ type: 'text', value: title }],
-        data: { _xdmExplicitJsx: true },
-      }
-
-      tree.children.splice(index, 0, titleNode)
-      // @ts-ignore
-      node.lang = language
     })
   }
 }
