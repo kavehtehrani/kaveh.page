a4a384b35e5ec5ae016e7c83c3529ab9f8272971
kavehtehrani <kaveh_tehrani@yahoo.com>
2025-07-11 15:48:42 +0700

    feat: update projects for danesh.app

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -16,7 +16,7 @@ export default function Projects() {
             className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900
             dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
           >
-            Projects
+            Personal Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{description}</p>
         </div>
