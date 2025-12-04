f58c69b2a1bf573fe2973036ce43b619952043ee
kavehtehrani <kaveh_tehrani@yahoo.com>
2025-12-04 18:02:13 -0300

    formatting

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -18,7 +18,7 @@ export default function ProjectsPage() {
           Growing list of independent projects done over the years
         </p>
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <div className="grid grid-cols-1 gap-4 pb-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {projectsData.map((project) => (
           <ProjectCard key={project.title} project={project} />
         ))}
