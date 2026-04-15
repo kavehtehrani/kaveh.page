534d8b49f9b41b71c5e715466bb1c4a3164e00c5
kavehtehrani <kaveh_tehrani@yahoo.com>
2026-04-15 21:00:14 +0800

    color change

--- app/(main)/snippets/page.tsx
+++ app/(main)/snippets/page.tsx
@@ -23,7 +23,7 @@ export default async function SnippetsPage() {
   return (
     <div>
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-terminal-orange-dim dark:text-terminal-orange sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Snippets
         </h1>
         <p className="text-lg leading-7 text-gray-500 dark:text-terminal-gray">
