d2d8044eb82f3d60b3326e8055dab7c5f5617fa2
kavehtehrani <kaveh_tehrani@yahoo.com>
2026-04-15 21:00:08 +0800

    color change

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -11,7 +11,7 @@ export default function ProjectsPage() {
   return (
     <div>
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-terminal-orange-dim dark:text-terminal-orange sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Personal Projects
         </h1>
         <p className="text-lg leading-7 text-gray-500 dark:text-terminal-gray">
--- components/blog/BlogClient.tsx
+++ components/blog/BlogClient.tsx
@@ -11,7 +11,7 @@ export function BlogClient({ posts }: { posts: BlogFrontMatter[] }) {
   return (
     <div className="divide-y divide-gray-200 dark:divide-terminal-bg-lighter">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 border-b border-gray-300 dark:border-terminal-bg-lighter pb-2">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-terminal-orange-dim dark:text-terminal-orange sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 border-b border-gray-300 dark:border-terminal-bg-lighter pb-2">
           Blog
         </h1>
         <p className="text-lg leading-7 text-gray-500 dark:text-terminal-gray">
